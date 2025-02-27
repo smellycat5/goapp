3c3e2fc8c354b1cd2f42d4bf30bc52358dc5ba8e
chunan.prajapati <chunan.prajapati@f1soft.com>
2025-02-27 17:52:45 +0545

    updated error handling

--- controllers/user/usersController.go
+++ controllers/user/usersController.go
@@ -4,13 +4,18 @@ import (
 	"Go/initializers"
 	"Go/models"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
 func Create(c *gin.Context) {
-	//get Data from request and create
 
-	user := models.User{Name: c.PostForm("name"), Email: c.PostForm("email")}
+	var user models.User
+
+	if err := c.ShouldBind(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+		return
+	}
 
 	result := initializers.DB.Create(&user)
 
@@ -19,18 +24,32 @@ func Create(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "User Created!"})
+	c.JSON(http.StatusOK, gin.H{"message": "User Created!!"})
 }
 
 func View(c *gin.Context) {
 	var user models.User
-	initializers.DB.First(&user, c.Param("id"))
+	result := initializers.DB.First(&user, c.Param("id"))
+
+	if result.Error != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{"data": user, "message": "User Found!"})
 }
 
 func Delete(c *gin.Context) {
-	initializers.DB.Delete(models.User{}, c.Param("id"))
+	var user models.User
+
+	result := initializers.DB.Delete(&user, c.Param("id"))
+
+	log.Printf("Rows Effected: %d", result.RowsAffected)
+
+	if result.Error != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "User Deleted!"})
 
